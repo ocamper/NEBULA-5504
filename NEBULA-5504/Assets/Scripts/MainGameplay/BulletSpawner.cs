c83a336bbf717ed567f3e9e69900885f9da25361
ocamper <danielocampolozano@gmail.com>
2023-05-20 18:19:10 -0500

    Reload mechanics added

--- NEBULA-5504/Assets/Scripts/MainGameplay/BulletSpawner.cs
+++ NEBULA-5504/Assets/Scripts/MainGameplay/BulletSpawner.cs
@@ -6,6 +6,11 @@ public class BulletSpawner : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
     [SerializeField] private float bulletForce;
+
+    [SerializeField] private HealthBarScript reloadBar;
+
+    private int cooldown;
+    private bool bulletReady = true;
     
     private GameObject bulletPrefab;
 
@@ -16,11 +21,34 @@ public class BulletSpawner : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1") && PlayerMovement.ActionAvailable)
+        if (Input.GetButtonDown("Fire1") && PlayerMovement.ActionAvailable && bulletReady)
         {
             GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
             rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+            bulletReady = false;
+            StartCoroutine(BulletCooldown());
+        }
+
+        if (bulletReady)
+            reloadBar.gameObject.SetActive(false);
+        else
+            reloadBar.gameObject.SetActive(true);
+    }
+
+    IEnumerator BulletCooldown()
+    {
+
+        reloadBar.SetHealth(0);
+
+        for (int i = 0; i < 10; i++)
+        {
+            yield return new WaitForSeconds(0.12f);
+
+            reloadBar.SetHealth(i);
+
+            if (i >= 9)
+                bulletReady = true;
         }
     }
 }
