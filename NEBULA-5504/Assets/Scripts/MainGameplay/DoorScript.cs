70e8473dafa2a83b93d1a5da0339a3c0769efc6f
ocamper <danielocampolozano@gmail.com>
2023-05-20 18:44:57 -0500

    Puzzle mechanics added (2nd dimension button fail)

--- NEBULA-5504/Assets/Scripts/MainGameplay/DoorScript.cs
+++ NEBULA-5504/Assets/Scripts/MainGameplay/DoorScript.cs
@@ -7,15 +7,35 @@ public class DoorScript : MonoBehaviour
     [SerializeField] private Animator doorAnim;
     [SerializeField] private bool doorOpen;
 
-    [SerializeField] private ButtonScript trigger;
+    [SerializeField] private ButtonScript button;
+    [SerializeField] private BulletTriggerButton altTrigger;
 
     private void Update()
     {
+        if (button != null && altTrigger == null)
+        {
+            if (button.buttonActive)
+                doorOpen = true;
+            else
+                doorOpen = false;
+        }
 
-        if (trigger.buttonActive)
-            doorOpen = true;
-        else
-            doorOpen = false;
+        if (altTrigger != null && button == null)
+        {
+            if (altTrigger.triggerActive)
+                doorOpen = true;
+            else
+                doorOpen = false;
+        }
+
+        if (altTrigger != null && button != null)
+        {
+            if (altTrigger.triggerActive && button.buttonActive)
+                doorOpen = true;
+            else
+                doorOpen = false;
+        }
+
 
         if (doorOpen)
             doorAnim.SetBool("doorOpen", true);
