635c119567efd78de2677a82c34ecc3816775957
ocamper <danielocampolozano@gmail.com>
2023-05-21 18:50:41 -0500

    Bullet Trigger sprite added

--- NEBULA-5504/Assets/Scripts/MainGameplay/Mechanics Scripts/BulletTriggerButton.cs
+++ NEBULA-5504/Assets/Scripts/MainGameplay/Mechanics Scripts/BulletTriggerButton.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class BulletTriggerButton : MonoBehaviour
 {
     public bool triggerActive;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite activeSprite;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Bullet")
         {
             triggerActive = true;
-            GetComponent<SpriteRenderer>().color = new Color(255f, 255f, 255f, 1f);
+            spriteRenderer.sprite = activeSprite;
         }
     }
 }
